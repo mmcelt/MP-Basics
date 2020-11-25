f764238efcb9cf140e1489ea0fbba40823360c34
Ron Welch <mmcelt@yahoo.com>
2020-11-24 20:59:42 -0700

    Initial Commit at 7 - Remote Actions

---
+++ Assets/Scripts/MyNetworkManager.cs
@@ -0,0 +1,39 @@
+﻿using Mirror;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyNetworkManager : NetworkManager
+{
+	#region Fields
+
+	MyNetworkPlayer _player;
+
+	#endregion
+
+	#region Mirror Callbacks
+
+	public override void OnServerAddPlayer(NetworkConnection conn)
+	{
+		base.OnServerAddPlayer(conn);
+
+		_player = conn.identity.GetComponent<MyNetworkPlayer>();
+
+		_player.SetDisplayName($"Player {numPlayers}");
+
+		Color randomColor = new Color(Random.Range(0f, 1f), Random.Range(0f, 1f), Random.Range(0f, 1f));
+
+		_player.SetDisplayColor(randomColor);
+	}
+	#endregion
+
+	#region Public Methods
+
+
+	#endregion
+
+	#region Private Methods
+
+
+	#endregion
+}
---
+++ Assets/Scripts/MyNetworkPlayer.cs
@@ -0,0 +1,68 @@
+﻿using Mirror;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class MyNetworkPlayer : NetworkBehaviour
+{
+	#region Fields
+
+	[SerializeField] TMP_Text _displayNameText;
+	[SerializeField] Renderer _displayColorRenderer;
+
+	[SerializeField] [SyncVar(hook = nameof(HandleDisplayNameUpdated))] string _displayName = "Missing Name";
+	[SerializeField] [SyncVar(hook = nameof(HandleDisplayColorUpdated))] Color _displayColor = Color.white;
+
+	#endregion
+
+	#region SyncVar Callbacks (Hooks)
+
+	void HandleDisplayNameUpdated(string oldName, string newName)
+	{
+		_displayNameText.SetText(newName);
+	}
+
+	void HandleDisplayColorUpdated(Color oldColor, Color newColor)
+	{
+		_displayColorRenderer.material.SetColor("_BaseColor", newColor);
+	}
+	#endregion
+
+	#region Server Methods
+
+	[Server]
+	public void SetDisplayName(string newName)
+	{
+		_displayName = newName;
+	}
+
+	[Server]
+	public void SetDisplayColor(Color newColor)
+	{
+		_displayColor = newColor;
+	}
+
+	[Command]
+	void CmdSetDisplayName(string newDisplayName)
+	{
+		RpcLogNewName(newDisplayName);
+		SetDisplayName(newDisplayName);
+	}
+	#endregion
+
+	#region Client Methods
+
+	[ContextMenu("Set My Name")]
+	void SetMyName()
+	{
+		CmdSetDisplayName("My New Name");
+	}
+
+	[ClientRpc]
+	void RpcLogNewName(string newName)
+	{
+		Debug.Log(newName);
+	}
+	#endregion
+}
